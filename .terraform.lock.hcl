9c8d66a455871a3bcdb44957e24dc1120a3fb9b5
Bogdan Peltea <mhibogdan@gmail.com>
2024-04-15 13:01:56 +0300

    Integrate cloud function 2nd gen

---
+++ .terraform.lock.hcl
@@ -0,0 +1,40 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/archive" {
+  version = "2.4.2"
+  hashes = [
+    "h1:1eOz9vM/55vnQjxk23RhnYga7PZq8n2rGxG+2Vx2s6w=",
+    "zh:08faed7c9f42d82bc3d406d0d9d4971e2d1c2d34eae268ad211b8aca57b7f758",
+    "zh:3564112ed2d097d7e0672378044a69b06642c326f6f1584d81c7cdd32ebf3a08",
+    "zh:53cd9afd223c15828c1916e68cb728d2be1cbccb9545568d6c2b122d0bac5102",
+    "zh:5ae4e41e3a1ce9d40b6458218a85bbde44f21723943982bca4a3b8bb7c103670",
+    "zh:5b65499218b315b96e95c5d3463ea6d7c66245b59461217c99eaa1611891cd2c",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7f45b35a8330bebd184c2545a41782ff58240ed6ba947274d9881dd5da44b02e",
+    "zh:87e67891033214e55cfead1391d68e6a3bf37993b7607753237e82aa3250bb71",
+    "zh:de3590d14037ad81fc5cedf7cfa44614a92452d7b39676289b704a962050bc5e",
+    "zh:e7e6f2ea567f2dbb3baa81c6203be69f9cd6aeeb01204fd93e3cf181e099b610",
+    "zh:fd24d03c89a7702628c2e5a3c732c0dede56fa75a08da4a1efe17b5f881c88e2",
+    "zh:febf4b7b5f3ff2adff0573ef6361f09b6638105111644bdebc0e4f575373935f",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google" {
+  version = "5.24.0"
+  hashes = [
+    "h1:pFxurRJS/rweAx8jqrz0AYRloNhvg/gL2G1dFX2QVa4=",
+    "zh:108d318bd6b5aa6cbfd4f5ace415c92129e37a78dd8ec2a1fe863a07690ac6ee",
+    "zh:12f2dad5c506934c07b655ae6de083374231acfff9b648de4fb8de2564d1c83c",
+    "zh:24bc487ea381a34c7416726f96307c9c30332b49a9937ea55ffb3092783f03b2",
+    "zh:2c0ead7c00f428da762ab0f848d19d6dc4cf1d1da8b8ed9eca560cb0abc0ff6b",
+    "zh:34b0a6f3ec8d1b1d6ae22f1ed00209a96c757f7f63fe9aebaab7a185bf79b66d",
+    "zh:4470e00308b80e071023771d1dc5b02ae249874e1a0fa29e2e701118126791c2",
+    "zh:581ebed948ef9b85d2683be8682cd2f2a2a3c8d0a260c2412197b6d1e9e3f2a7",
+    "zh:974c091fb381dc5a39afb447666221177cae75ba89c2884f02f12a1586a8b5b9",
+    "zh:a9d7a728c94144410f54360c3f8d92e8913b2564779f238cd420196ebf4014cc",
+    "zh:ba20867988cd386709639a2669fec7f3e086d075dc189555b389b7024efb779a",
+    "zh:e79d978f6ece3bb9d35bcabdc163803ff6c9fdffff8d733d431c9edbfe90e016",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
--- main.tf
+++ main.tf
@@ -6,63 +6,35 @@ resource "google_storage_bucket" "my_bucket" {
   public_access_prevention = "enforced"
 }
 
-# ## to_pdf_converter
-# resource "google_cloudfunctions2_function" "image_handler" {
-#   location = "europe-west3"
-#   name     = "image_handler"
+data "archive_file" "zip" {
+  type        = "zip"
+  source_dir  = "./cloud_functions/image_handler"
+  output_path = "./assets/function-image_handler.zip"
+}
 
 
-
-#   timeouts {
-#     create = "60m"
-#     update = "60m"
-#   }
-
-
-
-#   build_config {
-#     runtime     = "python38"
-#     entry_point = "image_handler"
-#     # docker_repository                        = "projects/docai-accelerator/locations/${var.region}/repositories/${var.cloudfunctions_artifactstorage_name}"
-#   }
-#   service_config {
-#     max_instance_count = 350
-#     available_memory   = "4G"
-#     available_cpu      = "4"
-#     timeout_seconds    = 540
-#   }
+resource "google_storage_bucket_object" "sourcecode" {
+  name   = "image_handler/function-source.zip"
+  bucket = "gcf-v2-sources-957891796445-europe-west3"
+  source = "./assets/function-image_handler.zip" # Add path to the zipped function source code
+}
 
 
-#   event_trigger {
-#     trigger_region = "europe-west3"
-#     event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
-#     pubsub_topic   = "projects/docai-accelerator/topics/image_handler_topic"
-#     retry_policy   = "RETRY_POLICY_DO_NOT_RETRY"
-#   }
-# }
-
-
-
-resource "google_cloudfunctions_function_v2" "image_handler" {
-  name        = "image-handler"
-  description = "Handles images"
-  region      = "europe-west3"
-
-  build_environment_variables = {
-    FUNCTION_SOURCE = "cloud_functions/image_handler"
-  }
+## to_pdf_converter
+resource "google_cloudfunctions2_function" "image_handler" {
+  location = "europe-west3"
+  name     = "image_handler"
+  project  = "docai-accelerator"
 
   timeouts {
     create = "60m"
     update = "60m"
   }
 
-  entry_point = "image_handler"
-  runtime     = "python38"
-
   build_config {
-    entry_point = "image_handler"
     runtime     = "python38"
+    entry_point = "image_handler"
+    # docker_repository                        = "projects/docai-accelerator/locations/${var.region}/repositories/${var.cloudfunctions_artifactstorage_name}"
     source {
       storage_source {
         bucket = "gcf-v2-sources-957891796445-europe-west3"
@@ -70,7 +42,6 @@ resource "google_cloudfunctions_function_v2" "image_handler" {
       }
     }
   }
-
   service_config {
     max_instance_count = 350
     available_memory   = "4G"
@@ -78,10 +49,16 @@ resource "google_cloudfunctions_function_v2" "image_handler" {
     timeout_seconds    = 540
   }
 
+
   event_trigger {
     trigger_region = "europe-west3"
     event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
     pubsub_topic   = "projects/docai-accelerator/topics/image_handler_topic"
     retry_policy   = "RETRY_POLICY_DO_NOT_RETRY"
   }
+
+  lifecycle {
+    replace_triggered_by = [ google_storage_bucket_object.sourcecode ]
+  }
+
 }
