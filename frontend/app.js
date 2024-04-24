6826cda86874651a427ce34b671e586f52d984a6
Bogdan Peltea <mhibogdan@gmail.com>
2024-04-24 17:25:01 +0300

    Frontend Implementation Added

---
+++ frontend/app.js
@@ -0,0 +1,155 @@
+const multer = require('multer');
+const express = require('express');
+
+const { chatHistory, updateDb, publishMessage, uploadFileToBucket, queryBigquery, registerDbUser } = require('./utils');
+
+
+
+const app = express();
+const session = require('express-session');
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+  secret: 'your_secret_key', // Choose a strong secret for session encoding
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: 'auto' } // secure: true in production with HTTPS
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', 'public/');
+
+// Initialize Socket.io
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+// Socket.io connection event
+io.on('connection', (socket) => {
+  console.log('A client connected');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Middleware to check if the user is authenticated
+function isAuthenticated(req, res, next) {
+  if (req.session && req.session.user) {
+      return next();
+  } else {
+      console.log("Unauthenticated request");
+      res.redirect('/login');
+  }
+}
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    rows = await queryBigquery(username, password);
+    // const user = users.find(u => u.username === username && u.password === password);
+    console.log(rows);
+    console.log(rows.length);
+    if (rows.length > 0 ) {
+        console.log('Login Succesful');
+        req.session.user = rows[0];  // Set the user in the session
+        // res.redirect('/');  // Redirect to the root URL after successful login
+        res.status(200).json({ success: true, redirectUrl: '/' });
+    } else {
+        console.log('Invalid username or password');
+        res.status(401).json({ success: false, message: 'Invalid username or password' });
+    }
+    } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const result = await registerDbUser(username, password);
+    if (result.success) {
+      res.status(201).json({ message: 'User registered successfully', userId: result.uuid });
+    } else {
+      res.status(500).json({ message: 'Registration failed', error: result.error });
+    }
+  } catch (error) {
+    console.error('Error in registration process:', error);
+    res.status(500).json({ message: 'Internal server error', error: error.message });
+  }
+});
+
+// Serve frontend
+app.get('/', isAuthenticated, async (req, res) => {
+  try {
+    const messages = await chatHistory(req.session.user.uuid);
+    console.log(messages);
+    res.render('index.ejs', { messages });
+  } catch (error) {
+    console.error('Error retrieving chat history:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Handle logout request
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+// Configure multer for handling file uploads
+const upload = multer({ dest: 'uploads/' });
+// Handle file uploads
+app.post('/upload', isAuthenticated, upload.single('file'), async (req, res) => {
+  try {
+    const textInput = req.body.textInput;
+    console.log(req.session.user.uuid);
+    const file = req.file; // Multer parses the file and stores the metadata in req.file
+    const postMessage = {};
+
+    if (!textInput && !file) {
+      return res.status(400).json({ message: 'No text input or file provided' });
+    }
+
+    if (textInput && !file) {
+      console.log(`Text input: ${textInput}`);
+      postMessage.statement = textInput;
+      postMessage.uuid = req.session.user.uuid;
+      await updateDb(postMessage.statement, postMessage.uuid);
+      await publishMessage('text_processor_trigger', JSON.stringify(postMessage));
+      res.json({ message: 'Upload successful', data: postMessage });
+    }
+    else if (file) {
+      console.log(`Processing file: ${file.originalname}`);
+      await uploadFileToBucket(file, "ingestion_data_placeholder", file.originalname);
+      postMessage.file_path = file.originalname;
+      if (textInput) {
+          postMessage.statement = textInput; // Ensure postMessage.statement is set only if textInput is provided
+          await updateDb(postMessage.statement, postMessage.uuid)
+      }
+      await publishMessage('format_classifier_trigger', JSON.stringify(postMessage));
+      res.json({ message: 'Upload successful', data: postMessage });
+    }
+  } catch (error) {
+    console.error('Error during upload:', error);
+    res.status(500).json({ message: 'Upload failed', error: error.message });
+  }
+});
+
+// Handle model response
+app.post('/model', async (req, res) => {
+  try {
+    const response = req.body.response;
+    const uuid = req.body.uuid;
+    console.log(`Response model: ${response}`);
+    await updateDb(response, uuid);
+    // await publishMessage('format_classifier_trigger', JSON.stringify(postMessage));
+    io.emit('notification', response);
+    res.status(200).json({ message: 'Notification sent successfully' });
+  } catch (error) {
+    console.error('Error sending notification:', error);
+    res.status(500).json({ message: 'Failed to send notification', error: error.message });
+  }
+});
+
+module.exports = server;
---
+++ frontend/public/index.js
@@ -0,0 +1,207 @@
+// Connect to the WebSocket server
+// const socket = io();
+//const socket = io('wss://hello-world-app-adwexujega-ey.a.run.app/socket.io/?EIO=4&transport=websocket', {transports: ['websocket', 'polling']});
+// Connect to the WebSocket server
+const socket = io('', {transports: ['websocket']});
+
+socket.on('connect_error', (error) => {
+    console.error('Connection Error:', error);
+});
+
+
+const options = {
+    timeZone: 'Europe/Bucharest',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  };
+
+function toggleLoadingScreen() {
+    const loadingScreen = document.getElementById('loadingScreen');
+    if (window.getComputedStyle(loadingScreen).display === 'flex') {
+        loadingScreen.style.display = 'none';
+    } else {
+        loadingScreen.style.display = 'flex';
+    }
+}
+
+// Event listener for WebSocket messages
+socket.on('notification', function(message) {
+    // Update the UI with the received message
+    toggleLoadingScreen();
+    const messageList = document.getElementById('messageList');
+    const newItem = document.createElement('li');
+    const timestamp = new Date().toLocaleTimeString('en-US', options);
+    newItem.textContent = `${timestamp} - ${message}`;
+    messageList.appendChild(newItem);
+});
+
+let fileUploaded = false; // Flag to track if a file is uploaded
+document.getElementById('uploadForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent form submission
+    const textInput = document.getElementById('textInput').value.trim(); // Trim whitespace from input
+    const files = document.getElementById('fileInput').files; // Get selected files
+    const timestamp = new Date().toLocaleTimeString('en-US', options);
+    // Clear the text input field
+    document.getElementById('textInput').value = '';
+
+    // Add the entered text to the list
+    if (textInput) {
+        const messageList = document.getElementById('messageList');
+        const listItem = document.createElement('li');
+        listItem.textContent = `${timestamp} - ${textInput}`;
+        messageList.appendChild(listItem);
+    }
+
+    // Upload files to Google Cloud Storage if a file is selected
+    console.log(files.length);
+    if (files.length === 1) {
+        try {
+            const formData = new FormData();
+            formData.append('textInput', textInput);
+            formData.append('file', files[0]); // Append only the first file
+
+            toggleLoadingScreen();
+            // Send a POST request with the form data
+            const response = await fetch('/upload', {
+                method: 'POST',
+                body: formData
+            });
+            const data = await response.json();
+            console.log(data);
+
+            // Hide file input and select file button after successful upload
+            hideFileInput();
+            fileUploaded = true; // Set fileUploaded flag to true
+        } catch (error) {
+            console.error('Error:', error);
+            toggleLoadingScreen();
+        }
+    } else {
+        console.log("Only text scenario");
+        // Handle the case where no file is selected
+        // Perform a POST request with the text input
+        try {
+            const message = {
+                textInput: textInput,
+            };
+            
+            // Convert the data object to a JSON string
+            const jsonData = JSON.stringify(message);
+            toggleLoadingScreen();
+            // Send a POST request with the JSON data
+            const response = await fetch('/upload', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: jsonData
+            });
+            const data = await response.json();
+            console.log(data);
+            
+        } catch (error) {
+            console.error('Error:', error);
+            toggleLoadingScreen();
+        }
+    }
+
+    // Handle text input (with or without file upload)
+    // Add your code to handle text input here, such as printing it or processing it in some way
+    console.log('Text Input:', textInput);
+});
+
+
+// Drag and drop functionality
+const dragDropBox = document.getElementById('drag-drop-box');
+const fileInfoText = document.getElementById('fileInfoText');
+const removeFileButton = document.getElementById('removeFileButton');
+const fileInput = document.getElementById('fileInput');
+const fileSelectButton = document.getElementById('fileSelectButton');
+
+dragDropBox.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    dragDropBox.classList.add('dragover');
+});
+
+dragDropBox.addEventListener('dragleave', () => {
+    dragDropBox.classList.remove('dragover');
+});
+
+dragDropBox.addEventListener('drop', (event) => {
+    event.preventDefault();
+    dragDropBox.classList.remove('dragover');
+
+    const files = event.dataTransfer.files;
+
+    // Check if more than one file is dropped
+    if (files.length !== 1) {
+        alert('Please drop only one file.'); // Display alert message
+        return; // Stop processing
+    }
+
+    handleFiles(files);
+
+    // Show drop message
+    fileInfoText.textContent = `File: ${files[0].name}`;
+    removeFileButton.style.display = 'inline-block'; // Display remove file button
+
+    // Hide file input and select file button
+    hideFileInput();
+});
+
+// Button to select files
+document.getElementById('fileSelectButton').addEventListener('click', () => {
+    fileInput.click();
+});
+
+// Button to remove file
+removeFileButton.addEventListener('click', () => {
+    fileInput.value = ''; // Clear selected file
+    fileInfoText.textContent = 'Drag and Drop Files Here'; // Reset file info text
+    removeFileButton.style.display = 'none'; // Hide remove file button
+
+    // Show file input and select file button
+    showFileInput();
+
+    // Reset fileUploaded flag to false
+    fileUploaded = false;
+});
+
+// File input change event listener
+fileInput.addEventListener('change', () => {
+    const files = fileInput.files;
+
+    // Check if more than one file is selected
+    if (files.length !== 1) {
+        alert('Please select only one file.'); // Display alert message
+        return; // Stop processing
+    }
+
+    handleFiles(files);
+
+    // Show file info text
+    fileInfoText.textContent = `File: ${files[0].name}`;
+    removeFileButton.style.display = 'inline-block'; // Display remove file button
+
+    // Hide file input and select file button
+    hideFileInput();
+});
+
+// Function to handle dropped or selected files
+function handleFiles(files) {
+    // Handle the dropped or selected files here (e.g., upload to server)
+    console.log('File:', files[0]);
+}
+
+// Function to hide file input and select file button
+function hideFileInput() {
+    fileInput.disabled = true; // Disable file input
+    fileSelectButton.style.display = 'none'; // Hide select file button
+}
+
+// Function to show file input and select file button
+function showFileInput() {
+    fileInput.disabled = false; // Enable file input
+    fileSelectButton.style.display = 'inline-block'; // Show select file button
+}
