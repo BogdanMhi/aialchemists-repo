fd21f3df6c454b103b31b1447a679aa5ce38a30f
alemnaru <alemnaru@deloittece.com>
2024-04-24 15:20:50 +0300

    final changes

--- terraform/variables_cloud_functions.tf
+++ terraform/variables_cloud_functions.tf
@@ -23,7 +23,7 @@ variable iot_handler_entry_point {default = "IoT_handler"}
 variable iot_handler_python_version {default = "python310"}
 
 ## text_processor
-variable text_processor_function_name {default = "text_processor_tf"}
+variable text_processor_function_name {default = "text_processor"}
 variable text_processor_function_memory {default = 4096}
 variable text_processor_entry_point {default = "text_processor"}
 variable text_processor_python_version {default = "python310"}
