c522a579a4e5aaf977ff3ff3e8bbee1a59a6d0f9
alemnaru <alemnaru@deloittece.com>
2024-04-24 11:14:15 +0300

    tf for gcs changes

--- terraform/deployment_gcs.tf
+++ terraform/deployment_gcs.tf
@@ -1,12 +1,10 @@
 ## Generate random number for bucket name
-resource "random_id" "ingestion_bucket_storage" {
-  byte_length = 3
-}
+resource "random_id" "ingestion_bucket_storage" { byte_length = 3 }
 
 ## Create GCS bucket
 resource "google_storage_bucket" "ingestion_bucket" {
   project                  = var.project
-  name                     = "${var.ingestion_data_bucket_name}_${lower(random_id.ingestion_bucket_storage.hex)}
+  name                     = "${var.ingestion_data_bucket_name}_${lower(random_id.ingestion_bucket_storage.hex)}"
   location                 = var.region
   force_destroy            = false
   public_access_prevention = "enforced"
