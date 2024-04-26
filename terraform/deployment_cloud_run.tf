e5998bc8901b69a4efafcdec2dc2cc7a3833eb78
alemnaru <alemnaru@deloittece.com>
2024-04-26 15:34:06 +0300

    cloud run changes

--- terraform/deployment_cloud_run.tf
+++ terraform/deployment_cloud_run.tf
@@ -1,31 +1,29 @@
 ## Cloud Run
 ## Activate Cloud Run API
-#resource "google_project_service" "cloudrun" {
-#  service  = "run.googleapis.com"
-#  project  = var.project
-#  disable_on_destroy = false
-#}
+resource "google_project_service" "cloudrun" {
+  service  = "run.googleapis.com"
+  project  = var.project
+  disable_on_destroy = false
+}
 
 ## Declare Cloud Run service
-resource "google_cloud_run_service" "web_app_test" {
+resource "google_cloud_run_v2_service" "web_app_test" {
   name     = var.cloud_run_web_app_name
   location = var.region
   project  = var.project
+  #ingress = "INGRESS_TRAFFIC_ALL"
+
   template {
-    spec {
-      containers {
-        image = "gcr.io/docai-accelerator/web-app"
-      }
+    containers {
+      image = "gcr.io/docai-accelerator/web-app"
     }
   }
 
   traffic {
-    percent         = 100
-    latest_revision = true
-    tag             = "latest_web_app"
+    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
   }
 }
-
 
 ## Set IAM policy to be publicly accessible
 resource "google_cloud_run_service_iam_member" "public" {
