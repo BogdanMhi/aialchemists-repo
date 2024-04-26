828740eebcb0d94e20b485a273d95b93736c5eb7
alemnaru <alemnaru@deloittece.com>
2024-04-26 18:41:32 +0300

    tf changes

--- terraform/deployment_cloud_run.tf
+++ terraform/deployment_cloud_run.tf
@@ -1,40 +1,40 @@
 ## Cloud Run
 ## Activate Cloud Run API
-resource "google_project_service" "cloud_run" {
-  service  = "run.googleapis.com"
-  project  = var.project
-  disable_on_destroy = false
-}
+#resource "google_project_service" "cloud_run" {
+#  service  = "run.googleapis.com"
+#  project  = var.project
+#  disable_on_destroy = false
+#}
 
 ## Declare Cloud Run service
-resource "google_cloud_run_v2_service" "web_app_test" {
-  name     = var.cloud_run_web_app_name
-  location = var.region
-  project  = var.project
+#resource "google_cloud_run_v2_service" "web_app_test" {
+#  name     = var.cloud_run_web_app_name
+#  location = var.region
+#  project  = var.project
   #ingress = "INGRESS_TRAFFIC_ALL"
 
-  template {
-    containers {
-      image = "europe-west3-docker.pkg.dev/docai-accelerator/cloud-run-source-deploy/app:latest"
-    }
-  }
+#  template {
+#    containers {
+#      image = "europe-west3-docker.pkg.dev/docai-accelerator/cloud-run-source-deploy/app:latest"
+#    }
+#  }
 
-  traffic {
-    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-    percent = 100
-  }
-}
+#  traffic {
+#    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+#    percent = 100
+#  }
+#}
 
 ## Set IAM policy to be publicly accessible
-resource "google_cloud_run_v2_service_iam_member" "public" {
-  project = google_cloud_run_v2_service.web_app_test.project
-  location = google_cloud_run_v2_service.web_app_test.location
-  name = google_cloud_run_v2_service.web_app_test.name
-  role = "roles/viewer"
-  member = "allUsers"
-}
+#resource "google_cloud_run_v2_service_iam_member" "public" {
+#  project = google_cloud_run_v2_service.web_app_test.project
+#  location = google_cloud_run_v2_service.web_app_test.location
+#  name = google_cloud_run_v2_service.web_app_test.name
+#  role = "roles/viewer"
+#  member = "allUsers"
+#}
 
 ## Exporting the URL
-output "url" {
-  value = "${google_cloud_run_v2_service.web_app_test.uri}"
-}
+#output "url" {
+#  value = "${google_cloud_run_v2_service.web_app_test.uri}"
+#}
