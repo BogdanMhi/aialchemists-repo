98d7377159753db24504d697302f2e081ecce573
alemnaru <alemnaru@deloittece.com>
2024-04-29 18:15:31 +0300

    update var project

--- terraform/deployment_artifact_registry.tf
+++ terraform/deployment_artifact_registry.tf
@@ -1,13 +1,13 @@
 # Enable Artifact Registry API
 #resource "google_project_service" "enable_artifact_registry_api" {
 #  service = "artifactregistry.googleapis.com"
-#  project = "<YOUR_PROJECT_ID>"
+#  project = var.project
 #}
 
 
 # Create an Artifact repository
 resource "google_artifact_registry_repository" "cf_repository" {
-  project       = var.project_id
+  project       = var.project
   location      = var.region
   repository_id = var.cloud_functions_repository_name
   format        = "DOCKER"
@@ -19,7 +19,7 @@ resource "google_artifact_registry_repository" "cf_repository" {
 #  repository = google_artifact_registry_repository.cf_repository.name
 #  location   = var.region
 #  role       = "roles/artifactregistry.writer"
-#  project    = var.project_id
+#  project    = var.project
 
 #  members = [
 #    "serviceAccount:${google_cloudfunctions_function.Cloud_function.service_account_email}"
