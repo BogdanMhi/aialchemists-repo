8b963c8be327c10cbb375366230e91f5da2a34c6
alemnaru <alemnaru@deloittece.com>
2024-04-30 18:23:17 +0300

    artifact registry permissions

--- terraform/deployment_artifact_registry.tf
+++ terraform/deployment_artifact_registry.tf
@@ -1,8 +1,8 @@
 # Enable Artifact Registry API
-#resource "google_project_service" "enable_artifact_registry_api" {
-#  service = "artifactregistry.googleapis.com"
-#  project = var.project
-#}
+resource "google_project_service" "enable_artifact_registry_api" {
+  service = "artifactregistry.googleapis.com"
+  project = var.project
+}
 
 
 # Create an Artifact repository
@@ -14,14 +14,12 @@ resource "google_artifact_registry_repository" "cf_repository" {
   description   = "Repository for cloud functions"
 }
 
-# Grant permissions for Cloud Function to push images
-#resource "google_artifact_registry_repository_iam_binding" "cloud_function_iam_binding" {
-#  repository = google_artifact_registry_repository.cf_repository.name
-#  location   = var.region
-#  role       = "roles/artifactregistry.writer"
-#  project    = var.project
-
-#  members = [
-#    "serviceAccount:${google_cloudfunctions_function.Cloud_function.service_account_email}"
-#  ]
-#}
+# Grant permissions to all users to push images
+resource "google_artifact_registry_repository_iam_binding" "cloud_function_iam_binding" {
+  repository = google_artifact_registry_repository.cf_repository.name
+  location   = var.region
+  role       = "roles/artifactregistry.writer"
+  project    = var.project
+  members    = ["allUsers",]
+  #members = ["serviceAccount:${google_cloudfunctions_function.Cloud_function.service_account_email}"]
+}
