c2c52c82e6b6ed0a0612636a24876cf3618bea06
alemnaru <alemnaru@deloittece.com>
2024-04-30 18:01:38 +0300

    docker changes

--- terraform/deployment_docker_images.tf
+++ terraform/deployment_docker_images.tf
@@ -1,22 +1,34 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "~> 3.0.1"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    docker = {
+#      source = "kreuzwerker/docker"
+#      version = "~> 3.0.1"
+#    }
+#  }
+#}
 
-provider "docker" {
-  registry_auth {
-    address  = "europe-west3-docker.pkg.dev"
-  }
-}
+#provider "docker" {
+#  registry_auth {
+#    address  = "europe-west3-docker.pkg.dev"
+#  }
+#}
 
-resource "docker_registry_image" "image_handler_image" {
-  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}"
+resource "docker_image" "image_handler_build" {
+  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}:1.0"
   build {
     context = "${var.image_handler_dockerfile_location}"
-    dockerfile = "Dockerfile"
   }
 }
+
+resource "docker_registry_image" "image_handler_registry_push" {
+  name          = docker_image.image_handler_build.name
+  keep_remotely = true
+}
+
+#resource "docker_registry_image" "image_handler_image" {
+#  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}"
+#  build {
+#    context = "${var.image_handler_dockerfile_location}"
+#    dockerfile = "Dockerfile"
+#  }
+#}
