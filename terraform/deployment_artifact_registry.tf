4b6374c2b901fe921ac599addc51cf6179f81743
alemnaru <alemnaru@deloittece.com>
2024-04-30 18:50:41 +0300

    add admin role for artifact registry

--- terraform/deployment_artifact_registry.tf
+++ terraform/deployment_artifact_registry.tf
@@ -17,7 +17,7 @@ resource "google_artifact_registry_repository" "cf_repository" {
 # Grant permissions to all users to push images
 data "google_iam_policy" "artifact_registry_editor" {
   binding {
-    role = "roles/artifactregistry.writer"
+    role = "roles/artifactregistry.admin"
     members = ["allUsers",]
   }
 }
