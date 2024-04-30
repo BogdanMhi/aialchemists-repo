9bd752bad02c9fb8606aff174fd60407e195ba5d
alemnaru <alemnaru@deloittece.com>
2024-04-30 19:25:23 +0300

    update for docker push

--- terraform/deployment_docker_images.tf
+++ terraform/deployment_docker_images.tf
@@ -21,7 +21,7 @@ resource "docker_image" "image_handler_build" {
 }
 
 resource "docker_registry_image" "image_handler_registry_push" {
-  name          = docker_image.image_handler_build.name
+  name          = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}:1.0"
   #keep_remotely = true
 }
 
