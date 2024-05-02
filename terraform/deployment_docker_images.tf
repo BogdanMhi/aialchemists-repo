0575b69169a69c8624026d91d4178801bed75a30
alemnaru <alemnaru@deloittece.com>
2024-05-02 10:17:57 +0300

    docker changes

--- terraform/deployment_docker_images.tf
+++ terraform/deployment_docker_images.tf
@@ -13,17 +13,17 @@ provider "docker" {
   }
 }
 
-resource "docker_image" "image_handler_build" {
-  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}:1.0"
-  build {
-    context = var.image_handler_dockerfile_location
-  }
-}
-
 resource "docker_registry_image" "image_handler_registry_push" {
   name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}:1.0"
   keep_remotely = true
 }
+
+#resource "docker_image" "image_handler_build" {
+#  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}:1.0"
+#  build {
+#    context = var.image_handler_dockerfile_location
+#  }
+#}
 
 #resource "docker_registry_image" "image_handler_image" {
 #  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}"
