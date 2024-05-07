fb2f106d32163881fcc23f060369d42f96bb63b4
alemnaru <alemnaru@deloittece.com>
2024-05-07 19:38:56 +0300

    docker changes

--- terraform/deployment_docker_images.tf
+++ terraform/deployment_docker_images.tf
@@ -1,10 +1,10 @@
 resource "docker_image" "image_handler_build" {
   provider = docker.docker_images
-  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}"
+  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}:latest_image_handler"
   build {
     context = var.image_handler_dockerfile_location
     dockerfile = "Dockerfile"
-    tag = ["${var.image_handler_docker_image}:latest_image_handler"]
+    #tag = ["${var.image_handler_docker_image}:latest_image_handler"]
   }
   triggers = {
     dir_sha1 = sha1(join("", [for f in fileset(path.module, "${var.image_handler_dockerfile_location}/*") : filesha1(f)]))
