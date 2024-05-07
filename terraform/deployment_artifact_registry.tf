943044bfebe80ee517eaf2b6ea1f6b0bcb2220cd
alemnaru <alemnaru@deloittece.com>
2024-05-07 19:00:18 +0300

    give permissions

--- terraform/deployment_artifact_registry.tf
+++ terraform/deployment_artifact_registry.tf
@@ -13,26 +13,18 @@ resource "google_artifact_registry_repository" "cf_repository" {
   description   = "Repository for cloud functions"
 }
 
-# Grant permissions to all users to push images
-#data "google_iam_policy" "artifact_registry_admin" {
-#  binding {
-#    role = "roles/artifactregistry.writer"
-#    members = ["allUsers",]
-#  }
-#}
-
-#resource "google_artifact_registry_repository_iam_policy" "ar_admin_policy" {
-#  location = var.region
-#  project = var.project
-#  repository = google_artifact_registry_repository.cf_repository.name
-#  policy_data = data.google_iam_policy.artifact_registry_admin.policy_data
-#}
-
 # Grant permissions for Cloud Function to push images
-resource "google_artifact_registry_repository_iam_binding" "cloud_function_iam_binding" {
-  location   = var.region
-  project    = var.project
+data "google_iam_policy" "artifact_registry_admin" {
+  binding {
+    role = "roles/artifactregistry.admin"
+    #members = ["allUsers",]
+    members = ["serviceAccount:${google_service_account.cloud_functions.email}"]
+  }
+}
+
+resource "google_artifact_registry_repository_iam_policy" "ar_admin_policy" {
+  location = var.region
+  project = var.project
   repository = google_artifact_registry_repository.cf_repository.name
-  role       = "roles/artifactregistry.writer"
-  members = ["serviceAccount:${google_service_account.cloud_functions.email}"]
+  policy_data = data.google_iam_policy.artifact_registry_admin.policy_data
 }
