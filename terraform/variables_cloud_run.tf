30a18e2aaf5b5a3dfcd48450992bcbaffe3a90c1
alemnaru <alemnaru@deloittece.com>
2024-05-08 12:27:09 +0300

    changes

--- terraform/variables_cloud_run.tf
+++ terraform/variables_cloud_run.tf
@@ -1,3 +1,3 @@
 ## Cloud Run web app
 variable cloud_run_web_app_name {default = "web-app-test"}
-variable cloud_run_image_handler_name {default = "image-handler-test"}
+variable cloud_run_image_handler_name {default = "image-handler"}
