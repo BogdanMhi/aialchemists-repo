2253fdab51745c2e6648f37d8e51b863052f2323
alemnaru <alemnaru@deloittece.com>
2024-05-08 12:58:46 +0300

    docker changes

--- terraform/deployment_docker_images.tf
+++ terraform/deployment_docker_images.tf
@@ -4,7 +4,7 @@ resource "docker_image" "image_handler_build" {
   build {
     context = var.image_handler_dockerfile_location
     dockerfile = "Dockerfile"
-    #tag = ["${var.image_handler_docker_image}:latest_image_handler"]
+    tag = ["${var.image_handler_docker_image}:V_001"]
   }
   triggers = {
     dir_sha1 = sha1(join("", [for f in fileset(path.module, "${var.image_handler_dockerfile_location}/*") : filesha1(f)]))
--- terraform/variables_docker_images.tf
+++ terraform/variables_docker_images.tf
@@ -1,3 +1,3 @@
 ## Cloud Run web app
-variable image_handler_docker_image {default = "image__handler"}
+variable image_handler_docker_image {default = "image_handler_tf"}
 variable image_handler_dockerfile_location {default = "../cloud_functions/image_handler"}
