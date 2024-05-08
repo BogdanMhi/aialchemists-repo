0d457a063ad6cd3b1e658971287e36133128315a
alemnaru <alemnaru@deloittece.com>
2024-05-08 10:42:44 +0300

    docker changes

--- terraform/deployment_docker_images.tf
+++ terraform/deployment_docker_images.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "image_handler_build" {
   provider = docker.docker_images
-  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}:latest_image_handler"
+  name = "europe-west3-docker.pkg.dev/${var.project}/${var.cloud_functions_repository_name}/${var.image_handler_docker_image}:latest"
   build {
     context = var.image_handler_dockerfile_location
     dockerfile = "Dockerfile"
