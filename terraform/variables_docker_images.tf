1a0eddb1420e2878485ba4030770fa65dded4f11
alemnaru <alemnaru@deloittece.com>
2024-05-09 14:52:17 +0300

    docker changes

--- terraform/variables_docker_images.tf
+++ terraform/variables_docker_images.tf
@@ -1,11 +1,11 @@
 ## document_handler
-variable document_handler_docker_image {default = "document_handler"}
+variable document_handler_docker_image {default = "document-handler"}
 variable document_handler_dockerfile_location {default = "../cloud_functions/document_handler"}
 
 ## image_handler
-variable image_handler_docker_image {default = "image_handler"}
+variable image_handler_docker_image {default = "image-handler"}
 variable image_handler_dockerfile_location {default = "../cloud_functions/image_handler"}
 
 ## video_handler
-variable video_handler_docker_image {default = "video_handler"}
+variable video_handler_docker_image {default = "video-handler"}
 variable video_handler_dockerfile_location {default = "../cloud_functions/video_handler"}
