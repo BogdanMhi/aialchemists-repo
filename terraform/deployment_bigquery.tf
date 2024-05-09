d709e1327b8cef8f560b6efa632869cb7fd4de18
alemnaru <alemnaru@deloittece.com>
2024-05-09 15:21:09 +0300

    integrate idempotency

--- terraform/deployment_bigquery.tf
+++ terraform/deployment_bigquery.tf
@@ -30,3 +30,61 @@ resource "google_bigquery_table" "bigquery_table" {
 ]
 EOF
 }
+
+## idempotency
+#dataset
+resource "google_bigquery_dataset" "bq_idempotency_dataset" {
+  dataset_id                  = var.bigquery_idempotency_dataset
+  friendly_name               = "AI Alchemists Idempotency Dataset"
+  project                     = var.project
+  location                    = var.region
+}
+
+#tables
+resource "google_bigquery_table" "bg_idempotency_document_table" {
+  dataset_id          = google_bigquery_dataset.bq_idempotency_dataset.dataset_id
+  deletion_protection = true
+  table_id            = var.bigquery_idempotency_document_handler
+  project             = var.project
+  schema              = <<EOF
+[
+  {
+    "name": "message_id",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  }
+]
+EOF
+}
+
+resource "google_bigquery_table" "bg_idempotency_image_table" {
+  dataset_id          = google_bigquery_dataset.bq_idempotency_dataset.dataset_id
+  deletion_protection = true
+  table_id            = var.bigquery_idempotency_image_handler
+  project             = var.project
+  schema              = <<EOF
+[
+  {
+    "name": "message_id",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  }
+]
+EOF
+}
+
+resource "google_bigquery_table" "bg_idempotency_video_table" {
+  dataset_id          = google_bigquery_dataset.bq_idempotency_dataset.dataset_id
+  deletion_protection = true
+  table_id            = var.bigquery_idempotency_video_handler
+  project             = var.project
+  schema              = <<EOF
+[
+  {
+    "name": "message_id",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  }
+]
+EOF
+}
--- terraform/variables_bigquery.tf
+++ terraform/variables_bigquery.tf
@@ -1,3 +1,9 @@
 ## bigquery_database
 variable bigquery_database_name {default = "ai_alchemists_user_table"}
 variable bigquery_database_table {default = "users"}
+
+## idempotency
+variable bigquery_idempotency_dataset {default = "idempotency_tf"}
+variable bigquery_idempotency_document_handler {default = "document_handler_msg_ids"}
+variable bigquery_idempotency_image_handler {default = "image_handler_msg_ids"}
+variable bigquery_idempotency_video_handler {default = "video_handler_msg_ids"}
