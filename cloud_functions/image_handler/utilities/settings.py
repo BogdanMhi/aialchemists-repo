e22e14590dd37d9c288e590ec2f536316402217a
alemnaru <159924264+alemnaru@users.noreply.github.com>
2024-05-10 17:28:33 +0300

    Update settings.py

--- cloud_functions/image_handler/utilities/settings.py
+++ cloud_functions/image_handler/utilities/settings.py
@@ -5,3 +5,6 @@ PROJECT_ID = os.environ.get("PROJECT_ID")
 
 # Pub/Sub
 TEXT_PROCESSOR_TRIGGER = os.environ.get("TEXT_PROCESSOR_TRIGGER")
+
+# Cloud Storage
+INGESTION_DATA_BUCKET = os.environ.get("INGESTION_DATA_BUCKET")
