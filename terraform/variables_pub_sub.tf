ad88c719f13bd0d0735497efcfdccc2c89f42cef
alemnaru <alemnaru@deloittece.com>
2024-05-10 14:18:04 +0300

    cloud run changes

--- terraform/variables_pub_sub.tf
+++ terraform/variables_pub_sub.tf
@@ -1,8 +1,8 @@
 ## Topics
-variable document_handler_topic_name {default = "document_handler_trigger"}
+variable document_handler_topic_name {default = "document_handler_trigger_tf"}
 variable format_classifier_topic_name {default = "format_classifier_trigger"}
-variable image_handler_topic_name {default = "image_handler_trigger"}
+variable image_handler_topic_name {default = "image_handler_trigger_tf"}
 variable iot_handler_topic_name {default = "iot_handler_trigger"}
 variable stats_generator_topic_name {default = "stats_generator_trigger"}
 variable text_processor_topic_name {default = "text_processor_trigger"}
-variable video_handler_topic_name {default = "video_handler_trigger"}
+variable video_handler_topic_name {default = "video_handler_trigger_tf"}
