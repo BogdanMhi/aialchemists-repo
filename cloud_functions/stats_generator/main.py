e4d6a77ced53834c24b90092e545330bb53731b4
alemnaru <alemnaru@deloittece.com>
2024-05-11 14:50:58 +0300

    stats update

--- cloud_functions/stats_generator/main.py
+++ cloud_functions/stats_generator/main.py
@@ -103,7 +103,7 @@ def stats_generator(event, context):
         Current conversation: {history}
         Human: Given the following information: {input} please focus on the main subjects and terms that are repeated or hold significant importance in the medical context that are related to medical field and are not generic or have a common understanding apart from the medical field. 
         Return the most frequent medical keywords formatted in a list of dictionaries with two keys: keywords and frequency where the values will represent the name of the keyword and the freqency of the keyword in the input. 
-        Return only the final list respecting the structure "[output]" without any additional text.
+        Return only the final list respecting the structure "[output]" without any additional text. All the words will be in title case.
     """
     prompt = PromptTemplate(input_variables=["input"], template=template)
     conversation = ConversationChain(prompt=prompt, llm=llm, verbose=True)
